1555ae07c37194108d5320a0c18f9be20f515e86
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-11-11 10:01:52 +0530

    Create bassicarray.cpp

---
+++ bassicarray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num[6]={35,23,46,21,78,92};
+    for(int&n: num){
+       
+    }
+   
+    cout<<"\n the number are: ";
+   
+        for(int i=0;i<5; ++i){
+            cout << num[i] <<", ";
+        }
+       
+        return 0;
+        }
