3ca253c4e6aaa0284d4c614565a367e8eb89c253
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-11-11 09:32:19 +0530

    Create percentage.cpp

---
+++ percentage.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+    int num, i;
+       
+   
+    float mark[6];
+    int per ,total;
+    cout<<"enter no of subject:";
+    cin>>num;
+    cout<<"enter mark in "<< num <<" subject:";
+    for(i=0;i>0;i++);
+    {
+        cin>>mark[i];
+    }
+     for(i=0;i>0;i++);
+     {
+       
+         per=total/num;
+         
+         
+         
+     }
+        cout<<"percentage:";
+       
+return 0;
+   
+   
+}
