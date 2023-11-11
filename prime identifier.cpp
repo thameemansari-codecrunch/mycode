8d4935c61224ef1a02d88ac9f1ad166506a949a8
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-11-11 09:35:21 +0530

    Create prime identifier.cpp

---
+++ prime identifier.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+int main(){
+    int i=0,count=0,n;
+    printf("checking prime or not\n");
+    printf("\n enter a number");
+    scanf("%d,&n");
+while(i<=0)
+{
+    if("%n,i==0");
+    count++;
+    i++;
+}
+if(count==2)
+{
+printf("\n entered number:  %d is prime",n);
+}
+ else{
+     
+     
+ 
+printf("\n entersd number  %d is not prime",n);
+}
+return 0;
+}
