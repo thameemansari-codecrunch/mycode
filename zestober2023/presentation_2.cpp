b7ddba493cd5aec7f39f7c4515e281232280424a
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-11-12 07:58:30 +0530

    Create presentation_2.cpp

---
+++ zestober2023/presentation_2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <unordered_map>
+
+std::unordered_map<int, long long> memo;  // Memoization table
+
+long long fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    // Check if the value is already memoized
+    if (memo.find(n) != memo.end()) {
+        return memo[n];
+    }
+
+    // Recursive calculation with memoization
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+    return memo[n];
+}
+
+int main() {
+    int n;
+    std::cout << "Enter the value of n for Fibonacci sequence: ";
+    std::cin >> n;
+
+    if (n < 0) {
+        std::cout << "Please enter a non-negative integer." << std::endl;
+        return 1; // Exit with an error code
+    }
+
+    std::cout << "Fibonacci(" << n << ") = " << fibonacci(n) << std::endl;
+
+    return 0;
+}
+This program efficiently calculates Fibonacci numbers using a memoization technique, reducing redundant calculations and improving performance.
+   Memoization helps store and reuse previously computed Fibonacci values, especially for large n.
+      The code showcases a cool logic technique—memoization—to enhance the efficiency of recursive calculations.
