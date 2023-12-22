edecbc117a689bcbaad0abaa775b7e5e84d8750d
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-12-22 12:15:00 +0530

    Update percentage.cpp

--- percentage.cpp
+++ percentage.cpp
@@ -1,30 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
-{  
-    int num, i;
-       
-   
-    float mark[6];
-    int per ,total;
-    cout<<"enter no of subject:";
-    cin>>num;
-    cout<<"enter mark in "<< num <<" subject:";
-    for(i=0;i>0;i++);
-    {
-        cin>>mark[i];
-    }
-     for(i=0;i>0;i++);
-     {
-       
-         per=total/num;
-         
-         
-         
-     }
-        cout<<"percentage:";
-       
-return 0;
-   
-   
+
+int main() {
+    // Declare variables
+    double totalMarks, obtainedMarks, percentage;
+
+    // Input total and obtained marks
+    cout << "Enter total marks: ";
+    cin >> totalMarks;
+
+    cout << "Enter obtained marks: ";
+    cin >> obtainedMarks;
+
+    // Calculate percentage
+    percentage = (obtainedMarks / totalMarks) * 100;
+
+    // Display the result
+    cout << "Percentage: " << percentage << "%" << endl;
+
+    return 0;
 }
