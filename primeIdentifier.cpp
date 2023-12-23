075787ea54d6030e25e7c6daeb9a5c52a8794a5c
thameemansari-codecrunch <150473958+thameemansari-codecrunch@users.noreply.github.com>
2023-12-23 14:05:27 +0530

    Update primeIdentifier.cpp

--- primeIdentifier.cpp
+++ primeIdentifier.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
-int main(){
-    int i=0,count=0,n;
-    printf("checking prime or not\n");
-    printf("\n enter a number");
-    scanf("%d,&n");
-while(i<=0)
-{
-    if("%n,i==0");
-    count++;
-    i++;
-}
-if(count==2)
-{
-printf("\n entered number:  %d is prime",n);
-}
- else{
-     
-     
- 
-printf("\n entersd number  %d is not prime",n);
-}
-return 0;
+
+int main() {
+    int i = 2, count = 0, n;
+
+    std::cout << "Checking prime or not\n";
+    std::cout << "Enter a number: ";
+    std::cin >> n;
+
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            count++;
+            break;
+        }
+        i++;
+    }
+
+    if (count == 0 && n != 1) {
+        std::cout << "Entered number " << n << " is prime." << std::endl;
+    } else {
+        std::cout << "Entered number " << n << " is not prime." << std::endl;
+    }
+
+    return 0;
 }
